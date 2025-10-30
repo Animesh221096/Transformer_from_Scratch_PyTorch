81a439a9b14bf7ec3e80d6508386f3a43f237c79
Animesh <animeshjain221096@gmail.com>
2025-10-30 18:43:44 +0530

    Implemented PositionalEncoding

--- model.py
+++ model.py
@@ -12,3 +12,31 @@ class InputEmbeddings(nn.Module):
         
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
+    
+    
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model: int = d_model
+        self.seq_len: int = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        
+        pe[:, ::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        pe.unsqueeze(0) # (1, seq_len, d_model)
+        
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape(1), :]).requires_grad_(False)
+        return self.dropout(x)
