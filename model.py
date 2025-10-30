349a18d67e8d408f4d05eb0f0cf332f1f54164dc
Animesh <animeshjain221096@gmail.com>
2025-10-30 19:03:46 +0530

    Implemented FeedForwardBlock

--- model.py
+++ model.py
@@ -56,3 +56,19 @@ class PositionalEncoding(nn.Module):
             return self.alpha * (x - mean) / (std + self.esp) + self.bias
         
         
+    class FeedForwardBlock(nn.Module):
+        
+        def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+            super().__init__()
+            self.linear_1 = nn.Linear(d_model, d_ff) # W1 and B1
+            self.dropout = nn.Dropout(dropout)
+            
+            self.linear_2 = nn.Linear(d_ff, d_model) # W2 and B2
+            
+        def forward(self, x):
+            # (Batch, Seq_Len, d_model) --> (Batch, Seq_Len, d_ff) --> (Batch, Seq_Len, d_model)
+            
+            return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+        
+        
+        
