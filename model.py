0130761d5b8f2b16b4112c6178a92db6c19e989c
Animesh <animeshjain221096@gmail.com>
2025-10-31 23:53:25 +0530

    Implemented Transformer

--- model.py
+++ model.py
@@ -209,3 +209,38 @@ class ProjectionLayer(nn.Module):
         return torch.log_softmax(self.proj(x), dim= -1)
     
     
+class Transformer(nn.Module):
+    
+    def __init__(self, encoder: Encoder, decoder: Decoder, 
+                 src_embed: InputEmbeddings,  tgt_embed: InputEmbeddings,
+                 src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, 
+                 projection_layer: ProjectionLayer) -> None:
+        super().__init__()
+        self.encoder: Encoder = encoder
+        self.decoder: Decoder = decoder
+        
+        self.src_embed: InputEmbeddings = src_embed
+        self.tgt_embed: InputEmbeddings = tgt_embed
+        
+        self.src_pos: PositionalEncoding = src_pos
+        self.tgt_pos: PositionalEncoding = tgt_pos
+        
+        self.projection_layer: ProjectionLayer = projection_layer
+        
+    
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+    
+    
+    def decode(self, encoder_output, src_mask, tgt, tgt_mask):
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+    
+    
+    def project(self, x):
+        return self.projection_layer(x)
+    
+    
