35bc88da43a74a70f2cf60a93003e81947d5003f
Animesh <animeshjain221096@gmail.com>
2025-11-01 17:09:41 +0530

    Implemented get_ds in train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from dataset import BilingualDataset, causal_mask
+
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
@@ -46,4 +48,25 @@ def get_ds(config):
     val_ds_size = len(ds_raw) - train_ds_size
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
     
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config["lang_src"], config["lang_tgt"], config["seq_len"])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config["lang_src"], config["lang_tgt"], config["seq_len"])
     
+    # Find the maximum length of each sentence in the source and target sentence
+    max_len_src = 0
+    max_len_tgt = 0
+    
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item["translation"][config["lang_src"]]).ids
+        tgt_ids = tokenizer_tgt.encode(item["translation"][config["lang_tgt"]]).ids
+        
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+        
+        print(f"Max length of the source sentence {max_len_src}")
+        print(f"Max length of the target sentence {max_len_tgt}")
+        
+    train_dataloader = DataLoader(train_ds, batch_size=config["batch_size"], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+        
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+        
