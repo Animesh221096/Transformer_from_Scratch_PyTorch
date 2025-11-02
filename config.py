e6a31c95f1063de77b223a5aa9abdb55bebe3b8b
Animesh <animeshjain221096@gmail.com>
2025-11-02 18:14:05 +0530

    Implemeted get_weights_file_path

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 def get_config():
     return {
         "batch_size": 8,
@@ -9,10 +11,17 @@ def get_config():
         "lang_src": "en",
         "lang_tgt": "it",
         "model_folder": "weights",
-        "model_filename": "tmodel_",
+        "model_basename": "tmodel_",
         "preload": None,
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/tmodel"
     }
     
     
+def get_weights_file_path(config, epoch: str):
+    model_folder = config["model_folder"]
+    
+    model_basename = config["model_basename"]    
+    model_filename = f"{model_basename}{epoch}.py"
+    
+    return str(Path('.') / model_folder / model_filename)
